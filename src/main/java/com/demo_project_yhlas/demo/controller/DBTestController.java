e265417942f231f44b22e683fa1dc16c633189db
Ihlasthetics <yhlascary04@gmail.com>
2025-11-04 13:20:07 +0100

    Add PostgreSQL connection and test endpoint

---
+++ src/main/java/com/demo_project_yhlas/demo/controller/DBTestController.java
@@ -0,0 +1,22 @@
+package com.demo_project_yhlas.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+public class DBTestController {
+
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/api/test/db")
+    String test() throws Exception{
+        try(var c = dataSource.getConnection()){
+            return "Connected to " + c.getMetaData().getURL();
+        }
+    }
+
+}
