86b2e47ef3046b9d133bff99ba5bf9ffb8f596fa
Ihlasthetics <yhlascary04@gmail.com>
2025-11-16 19:04:38 +0100

    Fix the issues

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email unique VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
